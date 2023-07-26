a4d02b712b822355db60be80229e1cf2b099a63d
developer-talaghay <developer@talaghay.com>
2023-07-27 04:23:42 +0800

    google signup api

--- server.js
+++ server.js
@@ -1,42 +1,57 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors"); // Import the CORS module
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
-const ejs = require('ejs');
-app.set('view engine', 'ejs');
 
-app.use((req, res, next) => {
-  console.log(`Requested URL: ${req.url}`);
-  next();
-});
-
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
-var corsOptions = {
-    origin: ["http://localhost:3000, http://18.191.79.11:8080"]
-  };
-  
-  app.use(cors(corsOptions));
+// Google OAuth
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get("/", (req, res) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: '1001534039958-8bfqeo9ls4mif2i0bt38hdr9t761d4ui.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: '/api/v1/google-signup/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Extract necessary user data from the Google profile
+      const user = {
+        email: profile.emails[0].value,
+        // Add any other necessary fields
+      };
+
+      // Handle the user data in the callback
+      done(null, user);
+    }
+  )
+);
+
+app.use(passport.initialize());
+
+// Routes
+const googleSignupRoute = require('./app/routes/googleSignUpRoutes');
+const userSignupRoute = require('./app/routes/userSignupRoutes');
+const userLoginRoute = require('./app/routes/userLoginRoutes');
+const userResetPasswordRoute = require('./app/routes/userResetPasswordRoutes');
+
+app.use('/api/v1/google-signup', googleSignupRoute);
+app.use('/api/v1/signup', userSignupRoute);
+app.use('/api/v1/login', userLoginRoute);
+app.use('/api/v1', userResetPasswordRoute);
+
+// Default route
+app.get('/', (req, res) => {
   res.send("It's working");
 });
 
-// routes should be here
-const userSignupRoute = require("./app/routes/userSignupRoutes");
-const userLoginRoute = require("./app/routes/userLoginRoutes");
-const userResetPasswordRoute = require("./app/routes/userResetPasswordRoutes");
-
-//middlewares here
-app.use("/api/v1/signup", userSignupRoute)
-app.use("/api/v1/login", userLoginRoute)
-app.use("/api/v1", userResetPasswordRoute)
-
-
-//port listening
+// Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-module.exports = app;
