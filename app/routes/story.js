f272c1c9390e0eb06c0e7389592e3ce9721e2581
developer-talaghay <developer@talaghay.com>
2023-08-09 21:53:03 +0800

    1 minute

--- app/routes/story.js
+++ app/routes/story.js
@@ -10,7 +10,7 @@ router.get('/fetch-and-insert', async (req, res) => {
   res.json({ message: 'Fetching stories and inserting into the database.' });
 });
 
-// Set up the cron job to run every 10 seconds
-cron.schedule('*/10 * * * * *', fetchAndInsertStory);
+// Set up the cron job to run every 1 minute
+cron.schedule('* * * * *', fetchAndInsertStory);
 
 module.exports = router;
