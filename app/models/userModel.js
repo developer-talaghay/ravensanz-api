91e6f59ea0690e9b4b39816a7ee6dd9ffe665713
developer-talaghay <developer@talaghay.com>
2023-10-19 20:36:00 +0800

    added api

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -135,7 +135,7 @@ UserDetails.checkEmailExistence = (email, callback) => {
 
   UserDetails.getUserDetailsByUserId = (user_id, callback) => {
     dbConn.query(
-      'SELECT  u.email_add, u.type, ud.* FROM user u LEFT JOIN user_details ud ON u.id = ud.user_id WHERE u.id = ?',
+      'SELECT u.id, u.status, u.type, u.email_add, ud.full_name, ud.display_name, ud.birth_date, ud.country, ud.phone_number, ud.modified_at, ud.created_at, ud.isAdmin, ud.isWriterVerified, ud.isEmailVerified, ud.writerApplicationStatus, ud.imageId, ud.wingsCount, ud.isSubscriber, ud.subscriptionExpirationDate, ud.isReadingModeOver18, ud.writerBadge, ud.readerBadge FROM user u LEFT JOIN user_details ud ON u.id = ud.user_id WHERE u.id = ?',
       [user_id],
       (error, result) => {
         if (error) {
@@ -147,5 +147,10 @@ UserDetails.checkEmailExistence = (email, callback) => {
       }
     );
   };
+  
+  
+  
+  
+  
 
 module.exports = UserDetails;
