fedd238f0d1d711fc56b74d3931280e96f180add
developer-talaghay <developer@talaghay.com>
2023-10-21 00:27:49 +0800

    updated response

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -56,28 +56,63 @@ exports.checkEmail = (req, res) => {
     });
   };
 
-  exports.getUser = (req, res) => {
-    const user_id = req.query.id; // Assuming the user_id is provided in the query parameter
-  
-    UserDetailsModel.getUserDetailsByUserId(user_id, (error, result) => {
-      if (error) {
-        console.error('Error retrieving user details: ', error);
-        return res.status(500).json({ message: 'Error retrieving user details' });
-      } else {
-        if (result.length > 0) {
-          // Assuming there is only one user with the given user_id
-          const userDetails = result[0];
-  
-          // Convert the birth_date to the desired format
-          userDetails.birth_date = formatBirthDate(userDetails.birth_date);
-  
-          return res.status(200).json({ message: 'User details retrieved successfully', data: userDetails });
-        } else {
-          return res.status(404).json({ message: 'User details not found' });
-        }
-      }
-    });
+// Define a function to format user details with default values
+function formatUserDetails(userDetails) {
+  if (!userDetails) {
+    return null;
+  }
+
+  return {
+    id: userDetails.id || null,
+    status: userDetails.status || "false",
+    type: userDetails.type || "app",
+    email_add: userDetails.email_add || "No email",
+    full_name: userDetails.full_name || "No name",
+    display_name: userDetails.display_name || "No name",
+    birth_date: userDetails.birth_date ? formatBirthDate(userDetails.birth_date) : "1970-01-01",
+    country: userDetails.country || "NA",
+    phone_number: userDetails.phone_number || "#########",
+    modified_at: userDetails.modified_at || "1970-01-01",
+    created_at: userDetails.created_at || "1970-01-01",
+    isAdmin: userDetails.isAdmin || 0,
+    isWriterVerified: userDetails.isWriterVerified || 0,
+    isEmailVerified: userDetails.isEmailVerified || 0,
+    writerApplicationStatus: userDetails.writerApplicationStatus || 0,
+    imageId: userDetails.imageId || 0,
+    wingsCount: userDetails.wingsCount || 0,
+    isSubscriber: userDetails.isSubscriber || 0,
+    subscriptionExpirationDate: userDetails.subscriptionExpirationDate || "1970-01-01",
+    isReadingModeOver18: userDetails.isReadingModeOver18 || 0,
+    writerBadge: userDetails.writerBadge || 0,
+    readerBadge: userDetails.readerBadge || 0,
   };
+}
+
+exports.getUser = (req, res) => {
+  const user_id = req.query.id; // Assuming the user_id is provided in the query parameter
+
+  UserDetailsModel.getUserDetailsByUserId(user_id, (error, result) => {
+    if (error) {
+      console.error('Error retrieving user details: ', error);
+      return res.status(500).json({ message: 'Error retrieving user details' });
+    } else {
+      if (result.length > 0) {
+        // Assuming there is only one user with the given user_id
+        const userDetails = result[0];
+
+        // Format user details with default values for null fields
+        const formattedUserDetails = formatUserDetails(userDetails);
+
+        return res.status(200).json({
+          message: 'User details retrieved successfully',
+          data: formattedUserDetails,
+        });
+      } else {
+        return res.status(404).json({ message: 'User details not found' });
+      }
+    }
+  });
+};
   
   // Helper function to format the birth_date
   function formatBirthDate(birthDate) {
